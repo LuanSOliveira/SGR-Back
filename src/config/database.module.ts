387cdbf09436b49dda22baf2a70feaef83d1a36d
Luan <l.u.a.n@hotmail.com>
2026-02-27 08:04:59 -0300

    config env

--- src/config/database.module.ts
+++ src/config/database.module.ts
@@ -1,18 +1,31 @@
-import 'dotenv/config';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'seu_usuario',
-      password: 'sua_senha',
-      database: 'seu_banco',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true, // CUIDADO: Use true apenas em desenvolvimento
+    ConfigModule.forRoot({
+      // Define qual arquivo .env carregar com base no NODE_ENV
+      envFilePath:
+        process.env.NODE_ENV === 'production' ? '.env' : '.develop.env',
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_DATABASE'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        // Synchronize apenas se não for produção
+        synchronize: configService.get<string>('NODE_ENV') !== 'production',
+        // synchronize: true,
+        autoLoadEntities: true,
+      }),
     }),
   ],
 })
